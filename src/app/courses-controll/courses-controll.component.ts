1e5097868ca4adc5a2fc9dc7406cb195ffddf643
soheyl bokaie <soheylmehrani@gmail.com>
2020-12-23 14:50:17 +0200

    main

--- src/app/courses-controll/courses-controll.component.ts
+++ src/app/courses-controll/courses-controll.component.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { HttpService } from '../http.service';
 import { ICourse } from '../Interfaces/Course-interface';
+import { UserService } from '../user.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-courses-controll',
@@ -16,21 +19,55 @@ export class CoursesControllComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private httpService: HttpService,
-    private http: HttpClient
+    private http: HttpClient,
+    private userService: UserService,
+    private toaster: ToastrService,
+    private router: Router,
+    private location: Location
   ) {}
+  base_url = this.httpService.base_url;
   courseId: string;
   course: ICourse;
+  idtoken: string;
 
   ngOnInit(): void {
     this.courseId = this.route.snapshot.params['id'];
     this.GetCourse();
+    this.get_Authorize();
   }
 
   GetCourse() {
     this.http
-      .get(this.httpService.base_url + 'api/GetCourse/' + this.courseId)
+      .get(this.base_url + 'api/GetCourse/' + this.courseId)
       .subscribe((params: ICourse) => {
         this.course = params[0];
       });
+  }
+
+  get_Authorize() {
+    this.userService.currentToken$.subscribe((res) => {
+      this.idtoken = res.token;
+    });
+  }
+
+  deleteCourse() {
+    this.http
+      .delete(this.base_url + 'deletecourse/' + this.courseId, {
+        headers: { Authorization: 'Bearer ' + this.idtoken },
+        responseType: 'text',
+      })
+      .subscribe(
+        (params) => {
+          this.toaster.info('course deleted!', 'deleted');
+          this.back();
+        },
+        (error) => {
+          this.toaster.error(error.error, 'error');
+        }
+      );
+  }
+
+  back() {
+    this.location.back();
   }
 }
